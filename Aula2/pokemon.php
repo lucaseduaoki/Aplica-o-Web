dc3daf54157902013879ab5a78a73fe6424005fa
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2025-03-21 15:38:21 -0300

    Create pokemon.php

---
+++ Aula2/pokemon.php
@@ -0,0 +1,34 @@
+<?php
+
+$pokemons = [
+    [
+        "imagem" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png",
+        "nome" => "Bulbasaur",
+        "tipo" => "Planta/Venenoso",
+        "link" => "https://www.pokemon.com/br/pokedex/bulbasaur"
+    ],
+    [
+        "imagem" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png",
+        "nome" => "Charmander",
+        "tipo" => "Fogo",
+        "link" => "https://www.pokemon.com/br/pokedex/charmander"
+    ],
+    [
+        "imagem" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png",
+        "nome" => "Squirtle",
+        "tipo" => "Água",
+        "link" => "https://www.pokemon.com/br/pokedex/squirtle"
+    ],
+    [
+        "imagem" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png",
+        "nome" => "Pikachu",
+        "tipo" => "Elétrico",
+        "link" => "https://www.pokemon.com/br/pokedex/pikachu"
+    ],
+    [
+        "imagem" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/150.png",
+        "nome" => "Mewtwo",
+        "tipo" => "Psíquico",
+        "link" => "https://www.pokemon.com/br/pokedex/mewtwo"
+    ]
+];                              
