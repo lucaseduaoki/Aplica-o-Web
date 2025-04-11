b51e69ccc538e3090b5e4dda8f406e53b38e12a6
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2025-04-11 15:44:17 -0300

    Update GET.php

--- Aula3/Atividade1/GET.php
+++ Aula3/Atividade1/GET.php
@@ -1,20 +1,9 @@
-<?php
+<?php 
 
-if (isset($_GET['n1']) && isset($_GET['n2']) && isset($_GET['n3'])) {
-   
-    $n1 = $_GET['n1'];
-    $n2 = $_GET['n2'];
-    $n3 = $_GET['n3'];
+    echo "Página aberta";
 
+    $num1 = $_GET['num1'];
+    $num2 = $_GET['num2'];
 
-    if (is_numeric($n1) && is_numeric($n2) && is_numeric($n3)) {
-   
-        $media = ($n1 + $n2 + $n3) / 3;
-        echo "A média aritmética é: " . $media;
-    } else {
-        echo "Por favor, insira valores numéricos válidos.";
-    }
-} else {
-    echo "Por favor, forneça os três números via URL. Exemplo: media_get.php?n1=10&n2=20&n3=30";
-}
+    $resultado = $num1 + $num2;
 ?>
