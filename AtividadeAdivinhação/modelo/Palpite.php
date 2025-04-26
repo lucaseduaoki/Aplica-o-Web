73f0ad4551894c65658698edbb420e450886e929
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2025-04-25 23:20:58 -0300

    Update Palpite.php

--- AtividadeAdivinhação/modelo/Palpite.php
+++ AtividadeAdivinhação/modelo/Palpite.php
@@ -3,5 +3,65 @@
 class Palpite{
     public $nome;
     public $image;
-    public $dica
+    public $dica;
+
+    public function __construct($nome, $image, $dica){
+        $this->nome = $nome;
+        $this->image = $image;
+        $this->dica = $dica;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome($nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of image
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set the value of image
+     */
+    public function setImage($image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dica
+     */
+    public function getDica()
+    {
+        return $this->dica;
+    }
+
+    /**
+     * Set the value of dica
+     */
+    public function setDica($dica): self
+    {
+        $this->dica = $dica;
+
+        return $this;
+    }
 }
