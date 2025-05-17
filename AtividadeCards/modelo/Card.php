1d00d92e26133cd9bfc4f2ac0bd5086c288fec6c
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2025-05-16 22:27:32 -0300

    Create Card.php

---
+++ AtividadeCards/modelo/Card.php
@@ -0,0 +1,101 @@
+<?php 
+
+class Card{
+
+    private $image;
+    private $nome;
+    private $marca;
+    private $preco;
+    private $descricao;
+
+
+    /**
+     * Get the value of image
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set the value of image
+     */
+    public function setImage($image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome($nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of marca
+     */
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * Set the value of marca
+     */
+    public function setMarca($marca): self
+    {
+        $this->marca = $marca;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of preco
+     */
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    /**
+     * Set the value of preco
+     */
+    public function setPreco($preco): self
+    {
+        $this->preco = $preco;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of descricao
+     */
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    /**
+     * Set the value of descricao
+     */
+    public function setDescricao($descricao): self
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+}
